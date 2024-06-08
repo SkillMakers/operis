44e41608d7ada6803b3b22c14b0902fa11efa172
ibouakl <imad.bouakl@gmail.com>
2024-06-08 11:42:26 +0200

    Update IT and use h2 for tests

--- operis-user/operis-user-service/src/test/java/com/operis/OperisUserServiceApplicationTests.java
+++ operis-user/operis-user-service/src/test/java/com/operis/OperisUserServiceApplicationTests.java
@@ -2,8 +2,11 @@ package com.operis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OperisUserServiceApplicationTests {
 
     @Test
