aa2a4fa01bcdd2697a625068f07c9f53678b783b
rolep <role.polanco@gmail.com>
2024-08-10 19:37:31 +0200

    Added new subscription module.

---
+++ operis-subscription/operis-subscription-service/src/main/java/com/operis/operis/subscription/service/OperisSubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.operis.operis.subscription.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperisSubscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperisSubscriptionServiceApplication.class, args);
+    }
+
+}
---
+++ operis-subscription/operis-subscription-service/src/test/java/com/operis/operis/subscription/service/OperisSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operis.operis.subscription.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperisSubscriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
