c0064d3fc66682584680f3cc95f4aee7d9c5f248
rolep <role.polanco@gmail.com>
2024-09-07 23:48:09 +0200

    Fixed subscription integration tests

--- operis-subscription/operis-subscription-service/src/test/java/com/operis/operis/subscription/service/OperisSubscriptionServiceApplicationTests.java
+++ operis-subscription/operis-subscription-service/src/test/java/com/operis/operis/subscription/service/OperisSubscriptionServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.operis.operis.subscription.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OperisSubscriptionServiceApplicationTests {
 
     @Test
