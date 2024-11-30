d5ed8b4f6ef5bdb68ab11f75555909d539de31fb
rolep <role.polanco@gmail.com>
2024-11-30 17:02:54 +0100

    Doc on create project payload

--- operis-project/operis-project-service/src/main/java/com/operis/project/core/service/adapter/in/rest/model/CreateProjectPayload.java
+++ operis-project/operis-project-service/src/main/java/com/operis/project/core/service/adapter/in/rest/model/CreateProjectPayload.java
@@ -2,11 +2,13 @@ package com.operis.project.core.service.adapter.in.rest.model;
 
 import com.operis.project.core.application.project.model.CreateProjectCommand;
 import com.operis.project.core.application.project.model.ProjectOwner;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateProjectPayload(
-        @NotBlank(message = "The name must not be null or empty") String name,
-        String description
+        @Schema(description = "Project name", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank String name,
+        @Schema(description = "Project description", requiredMode = Schema.RequiredMode.NOT_REQUIRED) String description
 ) {
     public CreateProjectCommand toCommand(String connectedUserEmail) {
         return new CreateProjectCommand(
